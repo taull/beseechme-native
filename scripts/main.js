4aefc77cdd30ec80ac234c667d33b7a80a14d181
jhath16 <hathaway.justin@gmail.com>
2015-09-25 12:45:34 -0400

    register routes work properly

--- scripts/main.js
+++ scripts/main.js
@@ -137,6 +137,39 @@ BeMe.Views.BusinessRegister = Parse.View.extend({
 		this.$el.html(this.template(this.model));
 		$('#application').append(this.el);
 		BeMe.renderedViews.push(this);
+	},
+
+	events: {
+		'submit' : 'register'
+	},
+
+	register: function (e) {
+		e.preventDefault();
+
+    var email = $('input[name="email"]').val(),
+     password = $('input[name="password"]').val(),
+     confirmPassword = $('input[name="confirm-password"]').val(),
+     businessName = $('input[name="business-name"]').val(),
+    firstName = $('input[name="first-name"]').val(),
+     lastName = $('input[name="last-name"]').val();
+
+
+    if (password == confirmPassword) {
+      Parse.User.signUp(email, password, {
+        businessName: businessName,
+        firstName: firstName,
+        lastName: lastName,
+        userType:"business"
+      }, {
+        success: function (e) {
+          console.log(e);
+        }, error: function (obj, error) {
+          alert("Error " + error.code + ": " + error.message);
+        }
+      });
+    } else {
+      alert('Passwords don\'t match');
+    }
 	}
 });
 
@@ -151,6 +184,39 @@ BeMe.Views.ConsumerRegister = Parse.View.extend({
 		this.$el.html(this.template(this.model));
 		$('#application').append(this.el);
 		BeMe.renderedViews.push(this);
+	},
+
+	events: {
+		'submit' : 'register'
+	},
+
+	register: function (e) {
+		e.preventDefault();
+
+    var email = $('input[name="email"]').val(),
+     password = $('input[name="password"]').val(),
+     confirmPassword = $('input[name="confirm-password"]').val(),
+    firstName = $('input[name="first-name"]').val(),
+     lastName = $('input[name="last-name"]').val();
+
+     console.log(email, password, confirmPassword, firstName, lastName);
+
+
+    if(password == confirmPassword) {
+      Parse.User.signUp(email, password, {
+        firstName: firstName,
+        lastName: lastName,
+        userType:"consumer"
+      }, {
+        success: function (e) {
+          console.log(e);
+        }, error: function (obj, error) {
+          alert("Error " + error.code + ": " + error.message);
+        }
+      });
+    } else {
+      alert('Passwords don\'t match');
+    }
 	}
 });
 
