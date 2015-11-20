c398bc34842728928f229e818b5849680330b239
Taylor Aull <aull.taylor@gmail.com>
2015-11-19 20:45:21 -0500

    dashboard map full width

--- scripts/main.js
+++ scripts/main.js
@@ -1766,10 +1766,16 @@ BeMe.Views.DashboardMap = BeMe.Views.DashboardBaseView.extend({
     this.template = _.template($('#dashboard-map-view').text());
     this.render();
     this.initializeMap();
+    this.initializeBarMarkers();
+    this.infoWindow = new google.maps.InfoWindow({
+      content:null,
+    });
   },
 
   initializeMap: function () {
-    var userLocation = {lat: Parse.User.current().get('location')._latitude, lng: Parse.User.current().get('location')._longitude};
+    var self = this;
+    var user = Parse.User.current();
+    var userLocation = {lat: user.get('location')._latitude, lng: user.get('location')._longitude};
     
     // Using this.map gives a reference to the map for the entire view
     this.map = new google.maps.Map(document.getElementById('map'), {
@@ -1781,6 +1787,49 @@ BeMe.Views.DashboardMap = BeMe.Views.DashboardBaseView.extend({
       position: userLocation,
       map: this.map,
       title: "You are here"
+    });
+
+    marker.addListener('click', function () {
+      var infoWindow = self.infoWindow;
+      infoWindow.setPosition(userLocation);
+      infoWindow.setContent('You are here');
+      infoWindow.open(self.map);
+    });
+  },
+
+  initializeBarMarkers: function () {
+    var self = this;
+    var user = Parse.User.current();
+
+    var query = new Parse.Query('User');
+    //CHANGE THE DISTANCE TO user.get('maxDistance') AFTER WE GET THE SLIDER WORKING
+    query.withinMiles("location", user.get('location'), 100);
+    query.equalTo('userType', 'business');
+    query.notEqualTo('businessName', user.get('businessName'));
+    query.find().then(function (bars) {
+
+      console.log(bars);
+      _.each(bars, function (i) {
+        var userLocation = i.get('location');
+        var position = {lat: userLocation._latitude, lng: userLocation._longitude};
+
+        var marker = new google.maps.Marker({
+          position:position,
+          map: self.map,
+          title: i.get('businessName'),
+        });
+
+        marker.addListener('click', function (markerObject) {
+          var infoWindow = self.infoWindow;
+          console.log(markerObject.content);
+
+          infoWindow.setPosition(position);
+          infoWindow.setContent("<a href='#business/" + i.get('handle') + "'>" + i.get('businessName') + "</a>");
+          infoWindow.open(self.map);
+        });
+
+      });
+
     });
   }
 })
