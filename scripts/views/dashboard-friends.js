50b4710049aa567a9df460e8c3b2707e1ffdc0cb
jhath16 <hathaway.justin@gmail.com>
2015-12-22 22:36:39 -0500

    friends feed now pulls in statuses from consumers the current user follows

--- scripts/views/dashboard-friends.js
+++ scripts/views/dashboard-friends.js
@@ -2,9 +2,28 @@ BeMe.Views.DashboardFriends = BeMe.Views.DashboardBaseView.extend({
 	initialize: function () {
 		this.template = _.template($('#dashboard-friends-view').text());
 		this.render();
+		this.usersFollowing = null;
+		this.loadFollowers();
+	},
+
+	loadFollowers: function () {
+		var self = this;
+		var query = Parse.User.current().relation('barsFollowing').query();
+		query.equalTo('userType', 'consumer');
+		query.find().then(function (e) {
+			self.usersFollowing = e;
+			self.loadFriendsFeed();
+		});
 	},
 
 	loadFriendsFeed: function () {
-		console.log('something');
+		var query = new Parse.Query('status');
+		query.containedIn('createdBy', this.usersFollowing);
+		query.descending('createdAt');
+		query.find().then(function (statuses) {
+			console.log(statuses);
+			var collection = new Parse.Collection(statuses);
+			new BeMe.Views.StatusListView({collection:collection,container:'.friends-feed'});
+		});
 	}
 });
